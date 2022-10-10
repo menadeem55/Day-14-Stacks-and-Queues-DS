b52b6049e8c77094c7d6d46d66987d770d9cf648
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-10-11 02:17:57 +0530

    [Added: EnQueue Element in stack]

---
+++ src/com/queue/ds/Queue.java
@@ -0,0 +1,28 @@
+package com.queue.ds;
+
+public class Queue {
+	
+			LinkedList list = new LinkedList();
+			
+			public void enQueue(Object data) {
+				
+				list.addFirst(data);
+				
+			}
+			
+			public void display() {
+				
+				list.display();
+			}
+	public static void main(String[] args) {
+		System.out.println("Welcome to Stack and Queue Data structure");
+		Queue queue = new Queue();
+		queue.enQueue(70);
+		queue.enQueue(30);
+		queue.enQueue(56);
+		queue.display();
+		
+		
+	}
+
+}
